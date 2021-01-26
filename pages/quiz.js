1332417b37146932359264bd1e9fa23130bacb95
DelevoperTheus <Theus03>
2021-01-26 18:26:54 -0300

    Arrumando página de Quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+export const Back = styled.body`
+  background-color:#1d3557;
+
+  h1{
+    margin: 8% auto;
+    text-align: center;
+    font: bold 50pt Segoe UI;
+    -webkit-text-stroke-width: 3px;
+    -webkit-text-stroke-color: #000;
+  }
+`;
+export const Emoji = styled.div`
+  font-size: 10vw;
+  text-align: center;
+`;
 
 export default function QuizPage(){
   return(
-    <div>
-      Página de Quiz
-    </div>
+    <Back>
+      <h1>
+        Página em Desenvolvimento....
+      </h1>
+      <marquee hspace="30%"><Emoji>🔨💻⚾</Emoji></marquee>
+    </Back>
   );
 }
